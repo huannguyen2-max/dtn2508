a5592f7ecf214065d08a22fdf793a05e68ce0816
huan.nguyen2 <nguyentronghuan24@gmail.com>
2026-03-10 19:48:11 +0700

    M1-1111: init project

---
+++ src/main/java/vn/test/vtibackenddtn2508spring/VtibackendDtn2508SpringApplication.java
@@ -0,0 +1,13 @@
+package vn.test.vtibackenddtn2508spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtibackendDtn2508SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtibackendDtn2508SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/test/vtibackenddtn2508spring/VtibackendDtn2508SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.test.vtibackenddtn2508spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtibackendDtn2508SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
