7aba05e27b78572fce9522fd043208433bd2184a
Elysia <dh52400377@student.stu.edu.vn>
2026-03-12 19:32:38 +0700

    modified 2

--- src/main/java/vn/test/vtibackenddtn2508spring/Login.java
+++ src/main/java/vn/test/vtibackenddtn2508spring/Login.java
@@ -1,7 +1,7 @@
 package vn.test.vtibackenddtn2508spring;
 
 public class Login {
-    private String username;
+    private String username2;
     private String otp;
     private String password;
 }
